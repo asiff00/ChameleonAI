7442b5ed003a3b92a26971b3aab50f2a4d3e754a
himisir <31031926+himisir@users.noreply.github.com>
2023-10-10 19:45:26 +0600

    Fix: Better exception handling.

--- brain.py
+++ brain.py
@@ -6,30 +6,23 @@ class ChatbotBackend:
         self.palm_api_key = palm_api_key
         self.configure_palm()
         self.response = None
-        self.model = True
 
     def configure_palm(self):
         try:
             palm.configure(api_key=self.palm_api_key)
-
         except:
-            print(f"INVALID API KEY: {self.palm_api_key}")
-            self.model = None
+            self.hasAPI = False
 
     def initialize(self, context, prompt, temperature=0.7):
         try:
-            if self.model is None:
-                return "NO API FOUND: Set up API first."
             self.response = palm.chat(context=context, messages=prompt)
             return self.response.last
         except:
-            return "Error: Check your internet connection"
+            return "No API Found - Please set up a valid API first."
 
     @retry.Retry()
-    def generate_text(self, prompt, temperature=0.5):
+    def generate_text(self, prompt, temperature=0.7):
         try:
-            if self.model is None:
-                return "NO API FOUND: Set up API first."
             return "Sorry, could you please rephrase it?" if self.response.reply(prompt).last is None else self.response.reply(prompt).last
         except:
-            return "Error: Check your internet connection"
+            return "Error - Invalid API or connectivity issue: Please check your API credentials or internet connection"
