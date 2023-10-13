77d6e192c160352c38b52e7a5f6a9a31b2d3a810
himisir <31031926+himisir@users.noreply.github.com>
2023-10-14 04:34:04 +0600

    Docs:Minor update

--- src/main/ui.py
+++ src/main/ui.py
@@ -321,7 +321,7 @@ class ChatbotApp:
 
         priming_label = ttk.Label(configure_window, text="Character")
         priming_label.grid(row=0, column=0, padx=10, sticky="w")
-        priming = "You are an AI assistant!"
+        priming = "I am ChameleonAI"
         if len(self.priming) < 2:
             self.priming = priming
         priming_entry = PlaceholderEntry(configure_window, self.priming)
@@ -330,7 +330,7 @@ class ChatbotApp:
         decorator_label = ttk.Label(configure_window, text="Personality")
         decorator_label.grid(row=2, column=0, padx=10, sticky="w")
 
-        decorator = "You are here to help!"
+        decorator = "I'm everything you've ever wanted!"
         if len(self.decorator) < 2:
             self.decorator = decorator
         decorator_entry = PlaceholderEntry(configure_window, self.decorator)
